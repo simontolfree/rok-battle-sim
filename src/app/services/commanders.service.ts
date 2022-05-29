9b5ce7d92e953a4559ba8cbf4b4eb600fa0b39fa
Simon Tolfree <56439044+simontolfree@users.noreply.github.com>
2022-05-29 17:42:44 +0100

    Outline for a commander

--- src/app/services/commanders.service.ts
+++ src/app/services/commanders.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from "rxjs";
 
+
+export interface Commander {
+
+  id: number;
+  title: string;
+  tier: string;
+  red: string,
+  yellow: string,
+  blue: string,
+  name:string,
+  talentTreetreeData:object;
+  commanderType:CommanderType;
+  /*
+  TODO:
+  Create array of the 3 talent types. ie. blue,yellow to leaship etc
+   */
+
+  //CommanderType: CommanderType[];
+
+}
+
+
+export interface CommanderType {
+  red: string,
+  yellow: string,
+  blue: string,
+}
 @Injectable({
   providedIn: 'root'
 })
 export class CommandersService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  getCommanders(): Observable<Commander[]> {
+    return this.http.get<Commander[]>('/data/commanders.json')
+  }
 }
