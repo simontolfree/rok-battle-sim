2b929cd177376527cc1f358518838d23b6fad1ae
Simon Tolfree <56439044+simontolfree@users.noreply.github.com>
2022-05-30 22:21:16 +0100

    Added forms module for inputs

--- src/app/views/commander-list/commander-list.module.ts
+++ src/app/views/commander-list/commander-list.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {
   AvatarModule,
@@ -40,7 +40,8 @@ import {CommanderListRoutingModule} from "./commander-list-routing.module";
     ChartjsModule,
     AvatarModule,
     TableModule,
-    WidgetsModule
+    WidgetsModule,
+    FormsModule
   ],
   declarations: [CommanderListComponent]
 })
